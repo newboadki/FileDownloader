c6b7a6b0bd04d7d45137e8e6f45f813889629b7e
Borja Arias Drake <borja.arias@gmail.com>
2012-03-06 16:58:55 +0100

    Added tests folder

---
+++ Tests/OCMock/OCMock.h
@@ -0,0 +1,10 @@
+//---------------------------------------------------------------------------------------
+//  $Id: OCMock.h 39 2009-04-09 05:31:28Z erik $
+//  Copyright (c) 2004-2008 by Mulle Kybernetik. See License file for details.
+//---------------------------------------------------------------------------------------
+
+#import "OCMockObject.h"
+#import "OCMockRecorder.h"
+#import "OCMConstraint.h"
+#import "OCMArg.h"
+#import "NSNotificationCenter+OCMAdditions.h"
